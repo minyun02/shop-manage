0aed08221dfbb53db3762a65a0c591789cd75659
min <myun02302@gmail.com>
2024-05-03 23:44:37 +0900

    사용하지 않는 도메인 삭제

--- src/main/java/com/shop/management/retailProducts/RetailProductsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shop.management.retailProducts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RetailProductsRepository extends JpaRepository<RetailProducts, Long> {
-}
